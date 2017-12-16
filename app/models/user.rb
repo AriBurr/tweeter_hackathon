f236c679edfb94025d9c2ae3d362ac096f965a66
Anthony Kelley <anthonymkelley@gmail.com>
2017-12-16 10:16:50 -0700

    create models and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_one :bio
+  has_many :posts
+  has_many :comments
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
